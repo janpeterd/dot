06fcb3f517eef91861a267d9a4a19a8baebaecd2
Jan-Peter <janpeter.dhalle@gmail.com>
2023-01-01 03:18:46 +0100

    Change Org files directory location

--- arch/emacs/.doom.d/config.el
+++ arch/emacs/.doom.d/config.el
@@ -50,7 +50,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Documents/Notes")
 (setq org-startup-with-inline-images t)
 
 
